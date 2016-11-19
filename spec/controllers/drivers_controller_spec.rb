6c1a037cf72f24417d3749866f2c0152d1a40ea7
Stepan Bolotnikov <stepan@voog.com>
2016-11-19 17:56:12 +0200

    Cleared up the tests - before all hooks are apparently not transactional 🙃

--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 RSpec.describe DriversController, type: :controller do
   describe 'GET show' do
-    before :all do
-      @driver = FactoryGirl.create(:driver)
-    end
     it 'assigns correct driver' do
+      @driver = FactoryGirl.create(:driver)
+
       get :show, :id => @driver.id
       expect(assigns(:driver)).to eq @driver
     end
     it 'renders json' do
+      @driver = FactoryGirl.create(:driver)
+
       get :show, :id => @driver.id
       expect(response.content_type).to eq 'application/json'
     end
     it 'returns 404 when driver doesn\'t exist' do
-      get :show, :id => @driver.id + 1
+      get :show, :id => 1
       expect(response.status).to eq 404
     end
   end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,48 +2,55 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
   describe 'GET index' do
-    before :all do
-      @driver1 = FactoryGirl.create(:driver)
-      @driver2 = FactoryGirl.create(:driver)
-      @order1 = FactoryGirl.create(:order)
-      @order2 = FactoryGirl.create(:order)
-      @order1.driver = @driver1
-      @order2.driver = @driver2
-      @order1.save!
-      @order2.save!
-    end
     it 'returns orders of the given driver' do
+      @driver1 = FactoryGirl.create(:driver)
+      @order1 = FactoryGirl.create(:order)
+      @order1.driver = @driver1
+      @order1.save!
+
       get :index, :driver_id => @driver1.id
       expect(assigns(:orders)).to include @order1
     end
     it 'does not return orders of other drivers' do
+      @driver1 = FactoryGirl.create(:driver)
+      @order1 = FactoryGirl.create(:order)
+      @order1.driver = @driver1
+      @order1.save!
+      @driver2 = FactoryGirl.create(:driver)
+      @order2 = FactoryGirl.create(:order)
+      @order2.driver = @driver2
+      @order2.save!
+
       get :index, :driver_id => @driver1.id
       expect(assigns(:orders)).not_to include @order2
     end
     it 'returns json' do
+      @driver1 = FactoryGirl.create(:driver)
+
       get :index, :driver_id => @driver1.id
       expect(response.content_type).to eq 'application/json'
     end
     it 'returns 404 if invalid driver id given' do
-      get :index, :driver_id => @driver2.id + 1
+      get :index, :driver_id => 1
       expect(response.status).to eq 404
     end
   end
 
   describe 'GET show' do
-    before :all do
-      @order = FactoryGirl.create(:order)
-    end
     it 'assigns correct order' do
+      @order = FactoryGirl.create(:order)
+
       get :show, :id => @order.id
       expect(assigns(:order)).to eq @order
     end
     it 'returns json' do
+      @order = FactoryGirl.create(:order)
+
       get :show, :id => @order.id
       expect(response.content_type).to eq 'application/json'
     end
     it 'returns 404 if invalid order id given' do
-      get :show, :id => @order.id + 1
+      get :show, :id => 1
       expect(response.status).to eq 404
     end
   end
