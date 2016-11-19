1d28ec070ddaee9f8d9aaa2b177a5feffdb4ed2b
Stepan Bolotnikov <stepan@voog.com>
2016-11-19 18:23:24 +0200

    Changed default value of driver status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119125747) do
+ActiveRecord::Schema.define(version: 20161119162147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20161119125747) do
     t.string   "car_number"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'assigns drivers' do
+    it 'assigns an available driver' do
+      @free_driver = FactoryGirl.create(:driver)
+      @free_driver.status = :available
+      @free_driver.save!
+      @order = FactoryGirl.create(:order)
+
+      expect(@order.driver).to eq @free_driver
+    end
+  end
 end
