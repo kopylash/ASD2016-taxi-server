ae9b8e1f635788d16ea0623e36db5c21daaeaae6
Stepan Bolotnikov <stepan@voog.com>
2016-11-19 17:01:32 +0200

    Implemented drivers#show

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,11 @@
 class DriversController < ApplicationController
+  def show
+    begin
+      @driver = Driver.find(params[:id])
+
+      render :json => {:driver => @driver}
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
 end
--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe DriversController, type: :controller do
-
+  describe 'GET show' do
+    before :all do
+      @driver = FactoryGirl.create(:driver)
+    end
+    it 'assigns correct driver' do
+      get :show, :id => @driver.id
+      expect(assigns(:driver)).to eq @driver
+    end
+    it 'renders json' do
+      get :show, :id => @driver.id
+      expect(response.content_type).to eq 'application/json'
+    end
+    it 'returns 404 when driver doesn\'t exist' do
+      get :show, :id => @driver.id + 1
+      expect(response.status).to eq 404
+    end
+  end
 end
