43999a779c4d0340f5cdca12cae9bdaf87f8b222
Stepan Bolotnikov <stepan@voog.com>
2016-11-19 15:42:01 +0200

    Implemented orders#index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,7 @@
 class OrdersController < ApplicationController
+  def index
+    @driver = Driver.find(params[:driver_id])
+    @orders = @driver.orders
+    render :json => @orders.to_json
+  end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
-
+  describe 'GET index' do
+    before :all do
+      @driver1 = Driver.create!(:name => 'Driver 1')
+      @driver2 = Driver.create!(:name => 'Driver 2')
+      @order1 = Order.create!(:client_name => 'Client 1')
+      @order2 = Order.create!(:client_name => 'Client 2')
+      @order1.driver = @driver1
+      @order2.driver = @driver2
+      @order1.save!
+      @order2.save!
+    end
+    it 'returns orders of the given driver' do
+      get :index, :driver_id => @driver1.id
+      expect(assigns(:orders)).to include @order1
+    end
+    it 'does not return orders of other drivers' do
+      get :index, :driver_id => @driver1.id
+      expect(assigns(:orders)).not_to include @order2
+    end
+    it 'returns json' do
+      get :index, :driver_id => @driver1.id
+      expect(response.content_type).to eq 'application/json'
+    end
+  end
 end
