f0bfb44d5a5bd612bbdba2bac8e4ea611c2b7c37
Stepan Bolotnikov <stepan@voog.com>
2016-12-12 19:35:47 +0200

    Fixed returning approximate time of trip to client
    added log to async jobs

--- app/jobs/complete_client_order_async_job.rb
+++ app/jobs/complete_client_order_async_job.rb
@@ -2,6 +2,7 @@ class CompleteClientOrderAsyncJob
   include SuckerPunch::Job
   def perform(client_number)
     message = {:completed => true}
+    puts "complete order: " + message.to_s
     Pusher.trigger(client_number, "order_completed", message)
   end
 end
--- app/jobs/respond_to_client_async_job.rb
+++ app/jobs/respond_to_client_async_job.rb
@@ -1,7 +1,8 @@
 class RespondToClientAsyncJob
   include SuckerPunch::Job
-  def perform(order, driver, client_number)
-    message = {:order => order, :driver => driver}
+  def perform(order, driver, client_number, travel_time)
+    message = {:order => order, :driver => driver, :travelTime => travel_time}
+    puts "respond to client: " + message.to_s
     Pusher.trigger(client_number, "order_accepted", message)
   end
 end
