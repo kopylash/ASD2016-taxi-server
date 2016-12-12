479bd8c3cf35d7209bf1eb4fab3dd4fac4590770
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2016-12-12 20:00:28 +0200

    logging to STDOUT

--- app/jobs/notify_drivers_async_job.rb
+++ app/jobs/notify_drivers_async_job.rb
@@ -2,7 +2,7 @@ class NotifyDriversAsyncJob
   include SuckerPunch::Job
   def perform(order, driver_ids)
     message = {:order => order, :id_list => driver_ids}
-    puts "notify drive: " + message.to_s
+    Rails.logger.debug "notify drive: " + message.to_s
     Pusher.trigger('orders', 'new_order', message)
   end
 end
--- app/jobs/respond_to_client_async_job.rb
+++ app/jobs/respond_to_client_async_job.rb
@@ -2,7 +2,7 @@ class RespondToClientAsyncJob
   include SuckerPunch::Job
   def perform(order, driver, client_number, travel_time)
     message = {:order => order, :driver => driver, :travelTime => travel_time}
-    puts "respond to client: " + message.to_s
+    Rails.logger.debug "respond to client: " + message.to_s
     Pusher.trigger(client_number, "order_accepted", message)
   end
 end
